d3bc522e9c5210efc122ff41a629a82d7601aabe
Vinicius Barbosa <viniciusmbarbosa@hotmail.com>
2022-07-31 18:39:50 -0300

    Páginas e rotas - React Router DOM

--- src/components/Button.tsx
+++
@@ -1,9 +0,0 @@
-import { ButtonContainter, ButtonVariant } from "./Button.styles";
-
-interface ButtonProps {
-  variant?: ButtonVariant;
-}
-
-export function Button({ variant = "primary" }: ButtonProps) {
-  return <ButtonContainter variant={variant}>Enviar</ButtonContainter>;
-}
